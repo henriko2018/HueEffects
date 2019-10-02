13eb121fd0992164dbd263cde32a85e008b98e78
Henrik Olsson <u.henrik.olsson@live.se>
2019-10-02 16:59:58 +0200

    Add empty console app

---
+++ XmasEffect/BuildWindowsContainer.bat
@@ -0,0 +1,1 @@
+docker build -f .\Dockerfile.Windows -t xmas-effect .
---
+++ XmasEffect/Program.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace XmasEffect
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options => { });
+        }
+    }
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Options
+    {
+        [Option('g', "light-group", Required = true, HelpText = "Name of the light group to control.")]
+        public string LightGroup { get; set; }
+
+        /// <summary>
+        /// Length of color cycle in seconds.
+        /// </summary>
+        [Option('c', "cycle-length", Required = false, Default = 60, HelpText = "Length of cycle in seconds")]
+        public int CycleLength { get; set; }
+    }
+}
