866f6f8fb646eff0f05a2234ba51f1b3ca96d91c
Henrik <u.henrik.olsson@live.se>
2019-10-08 06:12:34 +0200

    Handle cancel in timers

--- HueEffects.Web/EffectHandlers/WarmupHandler.cs
+++ HueEffects.Web/EffectHandlers/WarmupHandler.cs
@@ -66,27 +66,32 @@ namespace HueEffects.Web.EffectHandlers
 
         private async void OnTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            if (CancellationToken.IsCancellationRequested)
+            try
+            {
+                // Turn on first
+    #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                SwitchOn(_lightIds, _config.UseMinTemp);
+    #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+                // Warm up
+                // Calculate time between each step. We want to go from min to max during configured warm-up.
+                var msDelta = (int) (_config.TurnOnAt.TransitionTime.TotalMilliseconds / (_config.UseMaxTemp - _config.UseMinTemp));
+                for (var temp = _config.UseMinTemp; temp <= _config.UseMaxTemp && !CancellationToken.IsCancellationRequested; temp++)
+                {
+                    await Task.Delay(msDelta, CancellationToken);
+    #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    UpdateColorTemp(temp, _lightIds);
+    #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                }
+            }
+            catch (OperationCanceledException)
             {
                 StopTimers();
-                return;
             }
-
-            // Turn on first
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			SwitchOn(_lightIds, _config.UseMinTemp);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-
-			// Warm up
-			// Calculate time between each step. We want to go from min to max during configured warm-up.
-			var msDelta = (int) (_config.TurnOnAt.TransitionTime.TotalMilliseconds / (_config.UseMaxTemp - _config.UseMinTemp));
-            for (var temp = _config.UseMinTemp; temp <= _config.UseMaxTemp && !CancellationToken.IsCancellationRequested; temp++)
+            catch (Exception ex)
             {
-                await Task.Delay(msDelta, CancellationToken);
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-				UpdateColorTemp(temp, _lightIds);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			}
+                _logger.LogError(ex, $"Exception caught in {nameof(OnTimer_Elapsed)}");
+            }
 		}
 
         private void AddTurnOffTimer()
@@ -101,31 +106,36 @@ namespace HueEffects.Web.EffectHandlers
 
         private async void OffTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            if (CancellationToken.IsCancellationRequested)
+            try
+            {
+                // Cool down
+                // Calculate time between each step. We want to go from max to min during configured cool-down.
+                var msDelta = (int) (_config.TurnOffAt.TransitionTime.TotalMilliseconds / (_config.UseMaxTemp - _config.UseMinTemp));
+                for (var temp = _config.UseMaxTemp; temp >= _config.UseMinTemp && !CancellationToken.IsCancellationRequested; temp--)
+                {
+                    await Task.Delay(msDelta, CancellationToken);
+        #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    UpdateColorTemp(temp, _lightIds);
+        #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                }
+
+                // Turn off
+        #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                SwitchOff(_lightIds);
+        #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+                // Lastly, schedule new timers
+                AddTurnOnTimer();
+                AddTurnOffTimer();
+            }
+            catch (OperationCanceledException)
             {
                 StopTimers();
-                return;
             }
-
-            // Cool down
-            // Calculate time between each step. We want to go from max to min during configured cool-down.
-            var msDelta = (int) (_config.TurnOffAt.TransitionTime.TotalMilliseconds / (_config.UseMaxTemp - _config.UseMinTemp));
-            for (var temp = _config.UseMaxTemp; temp >= _config.UseMinTemp && !CancellationToken.IsCancellationRequested; temp--)
+            catch (Exception ex)
             {
-                await Task.Delay(msDelta, CancellationToken);
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-				UpdateColorTemp(temp, _lightIds);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			}
-
-			// Turn off
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-			SwitchOff(_lightIds);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-
-			// Lastly, schedule new timers
-			AddTurnOnTimer();
-			AddTurnOffTimer();
+                _logger.LogError(ex, $"Exception caught in {nameof(OffTimer_Elapsed)}");
+            }
 		}
     }
 }
