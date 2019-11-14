49b43140c5919b8eaa6a50e75a15390290dc4e04
Henrik Olsson <u.henrik.olsson@live.se>
2019-11-14 17:39:47 +0100

    Update integration tests

--- HueEffects.Web.IntegrationTests/HomeController/IndexTests.cs
+++ HueEffects.Web.IntegrationTests/HomeController/IndexTests.cs
@@ -2,16 +2,19 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace HueEffects.Web.IntegrationTests.HomeController
 {
     [Collection("Integration tests collection")]
-    public class IndexTests
+    public class IndexTests    
     {
+        private readonly ITestOutputHelper _testOutputHelper;
         private readonly HttpClient _httpClient;
 
-        public IndexTests(TestHostFixture testHostFixture)
+        public IndexTests(TestHostFixture testHostFixture, ITestOutputHelper testOutputHelper)
         {
+            _testOutputHelper = testOutputHelper;
             _httpClient = testHostFixture.Client;
         }
 
@@ -19,6 +22,7 @@ namespace HueEffects.Web.IntegrationTests.HomeController
         public async Task Returns_OK()
         {
             var response = await _httpClient.GetAsync("/");
+            _testOutputHelper.WriteLine(await response.Content.ReadAsStringAsync());
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
--- HueEffects.Web.IntegrationTests/TestHostFixture.cs
+++ HueEffects.Web.IntegrationTests/TestHostFixture.cs
@@ -1,5 +1,15 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Groups;
 using Xunit;
 
 namespace HueEffects.Web.IntegrationTests
@@ -7,7 +17,7 @@ namespace HueEffects.Web.IntegrationTests
     /// <summary>
     /// One instance of this will be created per test collection.
     /// </summary>
-    public class TestHostFixture : ICollectionFixture<CustomWebApplicationFactory>
+    public class TestHostFixture : ICollectionFixture<WebApplicationFactory<Startup>>
     {
         public readonly HttpClient Client;
         public readonly CustomWebApplicationFactory WebApplicationFactory;
@@ -15,8 +25,7 @@ namespace HueEffects.Web.IntegrationTests
         public TestHostFixture()
         {
             WebApplicationFactory = new CustomWebApplicationFactory();
-            Client = WebApplicationFactory.CreateClient(new WebApplicationFactoryClientOptions
-            { AllowAutoRedirect = false });
+            Client = WebApplicationFactory.CreateClient(new WebApplicationFactoryClientOptions{AllowAutoRedirect = false});
         }
     }
 
@@ -26,5 +35,39 @@ namespace HueEffects.Web.IntegrationTests
         // This class has no code, and is never created. Its purpose is simply
         // to be the place to apply [CollectionDefinition] and all the
         // ICollectionFixture<> interfaces.
+    }
+
+    public class CustomWebApplicationFactory : WebApplicationFactory<TestStartup>
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder()
+        {
+            return WebHost
+                .CreateDefaultBuilder(Array.Empty<string>())
+                .UseStartup<TestStartup>();
+        }
+    }
+
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+        
+        protected override void AddPhilipsHueClient(IServiceCollection services)
+        {
+            var mock = new Mock<ILocalHueClient>();
+            mock.Setup(client => client.GetGroupsAsync())
+                .ReturnsAsync(new[] { new Group { Id = "1", Name = "Group 1" } });
+            mock.Setup(client => client.GetGroupAsync(It.IsAny<string>()))
+                .ReturnsAsync((string groupId) => new Group { Id = groupId, Lights = new List<string> { "1", "2" } });
+            mock.Setup(client => client.GetLightAsync(It.IsAny<string>()))
+                .ReturnsAsync((string lightId) => new Light
+                {
+                    Id = lightId,
+                    Capabilities = new LightCapabilities
+                        { Control = new Control { ColorTemperature = new ColorTemperature() } }
+                });
+            services.AddSingleton(mock.Object);
+        }
     }
 }
